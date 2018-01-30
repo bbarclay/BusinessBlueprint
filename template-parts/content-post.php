ea59afa35db4415037274835efb6606476056aa4
julycabigas <july@businessblueprint.com>
2018-01-30 14:06:23 +0800

    banner changes

--- template-parts/content-post.php
+++ template-parts/content-post.php
@@ -1,6 +1,6 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?> itemscope itemprop="http://schema.org/BlogPosting">
-    <div class="banner banner-top"><a href="https://52ways.com/?utm_campaign=52W+Free.BB&utm_source=Website" target="_blank"><img src="http://businessblueprint.com.au/wp-content/uploads/2018/01/52-WAYS-Banner-720x100px-GRAPH-2.jpg" alt="52 ways" width="100%" height="auto"/></a></div>
+    <div class="banner banner-top"><a href="https://52ways.com/?utm_campaign=52W+Free.BB&utm_source=Website" target="_blank"><img src="http://businessblueprint.com.au/wp-content/uploads/2018/01/52-WAYS-Banner-720x100px-GRAPH-2-1.jpg" alt="52 ways" width="100%" height="auto"/></a></div>
 
 	<header class="entry__header">
 		<h1 class="entry__title" itemprop="headline">
