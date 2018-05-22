c63b85121f40e575a2d957a6ade1330ea0c3f921
julycabigas <july@businessblueprint.com>
2018-05-22 10:17:49 +0800

    add 52ways bonus page

--- page-52ways-bonus.php
+++ page-52ways-bonus.php
@@ -7,293 +7,78 @@ get_header();
 ?>
 
   <div id="content" class="content" role="main" itemscope itemprop="mainContentOfPage">
-      <?php 
-            get_template_part('partials/page-banner')
-      ?>
+
+        <?php 
+          $banner = wp_get_attachment_image_src( get_post_thumbnail_id(), 'full' );
+          $style = $banner ? 'style="background-image: url( ' . $banner[0] . ' );"' : '';
+        ?>
+
+      <header class="entry__header  page-banner" <?php echo $style; ?>>
+        <div class="container">
+         
+            <h1 class="entry__title" itemprop="headline">
+              <?php the_title() ?>
+            </h1>
+
+        </div>
+      </header>
+ 
 
       <div class="container">
 
         
          <?php the_content() ?>
          
-         <div class="book-summaries">
-           
-                <div class="book">
-                  <div class="cover">
-                      <img src="http://businessblueprint.com.au/wp-content/uploads/2015/06/E-MYTH.png" alt="" width="211" height="346" class="alignnone size-full wp-image-14584" />
-                  </div>
-                  <div class="info">
-                      <div>
-                        <h3 class="title">7 Habits of Highly Effective People </h3>
-                        <h4 class="author">By Stephen Covey</h4>
-                        
-                        <a href="" class="download-link">Download</a>
-                      </div>
-                  </div>
-                </div>
+           <?php 
+
+           if( have_rows('books') ) : ?>
+
+              <div class="book-summaries">
+
+              <?php
+
+                while( have_rows('books') ) : the_row(); 
+
+              ?>
              
-                <div class="book">
-                  <div class="cover">
-                      <img src="http://businessblueprint.com.au/wp-content/uploads/2015/06/GOOD-TO-GREAT.png" alt="" width="211" height="346" class="alignnone size-full wp-image-14585" />
-                  </div>
-                  <div class="info">
-                      <div>
-                        <h3 class="title">Good To Great </h3>
-                        <h4 class="author">By Jim Collins</h4>
-                        
 
-                        <a href="" class="download-link">Download</a>
-                      </div>
-                  </div>
-                </div>
-                <div class="book">
-                  <div class="cover">
-                      <img src="http://businessblueprint.com.au/wp-content/uploads/2015/06/SEVEN-DAY-WEEKEND.png" alt="" width="211" height="346" class="alignnone size-full wp-image-14586" />
-                  </div>
-                  <div class="info">
-                      <div>
-                        <h3 class="title">The Seven Day Weekend </h3>
-                        <h4 class="author">by Ricardo Semler</h4>
-                        
+                  <div class="book">
+                    <div class="cover">
+                        <?php $image = get_sub_field('image');
+                              $size  = 'full';
 
-                        <a href="" class="download-link">Download</a>
-                      </div>
-                  </div>
-                </div>
-                <div class="book">
-                  <div class="cover">
-                      <img src="http://businessblueprint.com.au/wp-content/uploads/2015/06/GOOD-TO-GREAT.png" alt="" width="211" height="346" class="alignnone size-full wp-image-14585" />
-                  </div>
-                  <div class="info">
-                      <div>
-                        <h3 class="title">Good To Great </h3>
-                        <h4 class="author">By Jim Collins</h4>
-                        
+                              if( $image ) {
 
-                        <a href="" class="download-link">Download</a>
-                      </div>
+                                 echo  wp_get_attachment_image($image['id'], $size);
+                              }
+                         ?>
+                    </div>
+                    <div class="info">
+                        <div>
+                          <?php if( get_sub_field('title') ) : ?>
+                            <h3 class="title"><?php echo get_sub_field('title') ?></h3>
+                          <?php endif; ?>  
+                          <?php if( get_sub_field('author') ) : ?>
+                            <h4 class="author"><?php echo get_sub_field('author') ?></h4>
+                          <?php endif; ?>  
+                          <?php if( get_sub_field('download_link') ) : ?>
+                            <a href="<?php echo get_sub_field('download_link'); ?>" class="download-link" target="blank" download>Download</a>
+                          <?php endif; ?>  
+                          
+                        </div>
+                    </div>
                   </div>
-                </div>
-  
-                <div class="book">
-                  <div class="cover">
-                      <img src="http://businessblueprint.com.au/wp-content/uploads/2015/06/E-MYTH.png" alt="" width="211" height="346" class="alignnone size-full wp-image-14584" />
-                  </div>
-                  <div class="info">
-                      <div>
-                        <h3 class="title">7 Habits of Highly Effective People </h3>
-                        <h4 class="author">by Stephen Covey</h4>
-                        
-                        <a href="" class="download-link">Download</a>
-                      </div>
-                  </div>
-                </div>
-             
-                <div class="book">
-                  <div class="cover">
-                      <img src="http://businessblueprint.com.au/wp-content/uploads/2015/06/GOOD-TO-GREAT.png" alt="" width="211" height="346" class="alignnone size-full wp-image-14585" />
-                  </div>
-                  <div class="info">
-                      <div>
-                        <h3 class="title">Good To Great </h3>
-                        <h4 class="author">by Jim Collins</h4>
-                        
 
-                        <a href="" class="download-link">Download</a>
-                      </div>
-                  </div>
-                </div>
-                <div class="book">
-                  <div class="cover">
-                      <img src="http://businessblueprint.com.au/wp-content/uploads/2015/06/SEVEN-DAY-WEEKEND.png" alt="" width="211" height="346" class="alignnone size-full wp-image-14586" />
-                  </div>
-                  <div class="info">
-                      <div>
-                        <h3 class="title">The Seven Day Weekend </h3>
-                        <h4 class="author">by Ricardo Semler</h4>
-                        
-
-                        <a href="" class="download-link">Download</a>
-                      </div>
-                  </div>
-                </div>
-                <div class="book">
-                  <div class="cover">
-                      <img src="http://businessblueprint.com.au/wp-content/uploads/2015/06/GOOD-TO-GREAT.png" alt="" width="211" height="346" class="alignnone size-full wp-image-14585" />
-                  </div>
-                  <div class="info">
-                      <div>
-                        <h3 class="title">Good To Great </h3>
-                        <h4 class="author">By Jim Collins</h4>
-                        
-
-                        <a href="" class="download-link">Download</a>
-                      </div>
-                  </div>
-                </div>
-
-
-
-          </div>  
-
-<!--          <div class="book-summaries">
-            
-                <div class="book">
-                  <div class="cover">
-                      <img src="http://businessblueprint.com.au/wp-content/uploads/2015/06/E-MYTH.png" alt="" width="211" height="346" class="alignnone size-full wp-image-14584" />
-                  </div>
-                  <div class="info">
-                      <div>
-                        <h3 class="title">7 Habits of Highly Effective People </h3>
-                        <h4 class="author">By Stephen Covey</h4>
-                        
-                        <p>To download this bonus, right click below and “save target as”:</p>
-                        <div class="book-label">(Printed Book Summary)</div>
-                        <a href="" class="download-link">Download</a>
-                      </div>
-                  </div>
-                </div>
-
-                <div class="book">
-                  <div class="cover">
-                      <img src="http://businessblueprint.com.au/wp-content/uploads/2015/06/GOOD-TO-GREAT.png" alt="" width="211" height="346" class="alignnone size-full wp-image-14585" />
-                  </div>
-                  <div class="info">
-                      <div>
-                        <h3 class="title">Good To Great </h3>
-                        <h4 class="author">By Jim Collins</h4>
-                        
-                        <p>To download this bonus, right click below and “save target as”:</p>
-                        <div class="book-label">(Printed Book Summary)</div>
-                        <a href="" class="download-link">Download</a>
-                      </div>
-                  </div>
-                </div>
-        
-          </div>   -->    
+                <?php
+                endwhile;
+                ?>
+            </div>  
+            <?php
+            endif;
+            ?>    
 
       </div>
     
   </div>
-	<style>
-/*  .book {
-    margin-bottom: 20px;
-    border-bottom: 1px solid #e4e1e1;
-    display: flex;
-    align-items: center;
-   }
-   .book:after {
-     content: '';
-     clear: both;
-     display: block;
-   }
-   .book .cover {
-     width: 200px;
-     float: left;
-   }
-   .book .info {
-     width: calc(100% - 200px);
-     float: right;
-     padding-left: 30px;
-   }
-   .book .download-link {
-      color: #ffffff;
-      font-size: 19px;
-      font-family: 'Geogrotesque-SemiBold', sans-serif;
-      display: inline-block;
-      padding: 10px 20px;
-      background: #79c142;
-      border-radius: 4px;
-   } 
-   .book .download-link:hover {
-      color: #ffffff;
-      text-decoration: none;
-      background: #002749;
-   } 
-   .book .title {
-      margin-bottom: 0;
-      font-family: 'Geogrotesque-SemiBold', sans-serif;
-      font-size: 30px;
-   }
-   .book .author {
-     margin: 0 0 20px 0;
-     font-size: 24px;
-   }
-   .book-label {
-        font-style: italic;
-        font-size: 17px;
-        margin-bottom: 20px;
-    }*/
-    .page-banner {
-        background-size: cover;
-        background-repeat: no-repeat;
-        background-position: center center;
-    }
-    .book-summaries {
-      margin-bottom: 20px;
-      display: flex;
-      flex-wrap: wrap;
-      margin-left: -30px;
-      margin-right: -30px;
-    }
-    .book {
-        width: 25%;
-        padding: 0 30px;
-        margin-bottom: 35px;
-    }
-    .book img {
-      width: 100%;
-      box-shadow: 0 5px 30px #e0dcdc;
-    }
-    .book .download-link {
-      color: #ffffff;
-      font-size: 19px;
-      font-family: 'Geogrotesque-SemiBold', sans-serif;
-      display: inline-block;
-      padding: 5px 15px;
-      background-color: #79c142;
-      background: #7ac143 -webkit-linear-gradient(top,#7ac143 0,#639c37 100%);
-      background: #7ac143 linear-gradient(to bottom,#7ac143 0,#639c37 100%);
-      border-radius: 4px;
-   } 
-   .book .download-link:hover {
-      color: #ffffff;
-      text-decoration: none;
-      background: #002749;
-   } 
-   .book .title {
-      margin-bottom: 0;
-      font-family: 'Geogrotesque-SemiBold', sans-serif;
-      font-size: 20px;
-   }
-   .book .author {
-     margin: 0 0 10px 0;
-     font-size: 17px;
-   }
-   .book-label {
-        display: none;
-    }
-
-    @media ( max-width: 991px) {
-        .book-summaries {
-            margin-left: -30px;
-            margin-right: -30px;
-        }
-        .book {
-           width: 33.3333%;
-        }
-    }
-
-    @media ( max-width: 767px) {
-        .book-summaries {
-            margin-left: -15px;
-            margin-right: -15px;
-        }
-        .book {
-           width: 50%;
-           padding: 0 15px;
-        }
-    }
-  </style>
 
 <?php get_footer(); ?>
